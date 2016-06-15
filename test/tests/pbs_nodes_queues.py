93b7704bc5aa5dbc41c1d0a9ebcbfd3fc9ecf890
Bhroam Mann <bmann@altair.com>
2016-06-14 17:50:54 -0700

    PP-270: Node/queue association is ignored when node_group_key is only set at the server level

---
+++ test/tests/pbs_nodes_queues.py
@@ -0,0 +1,92 @@
+# coding: utf-8
+
+# Copyright (C) 1994-2016 Altair Engineering, Inc.
+# For more information, contact Altair at www.altair.com.
+# 
+# This file is part of the PBS Professional ("PBS Pro") software.
+#
+# Open Source License Information:
+# 
+# PBS Pro is free software. You can redistribute it and/or modify it under the
+# terms of the GNU Affero General Public License as published by the Free 
+# Software Foundation, either version 3 of the License, or (at your option) any 
+# later version.
+# 
+# PBS Pro is distributed in the hope that it will be useful, but WITHOUT ANY 
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details.
+# 
+# You should have received a copy of the GNU Affero General Public License along 
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+# 
+# Commercial License Information: 
+#
+# The PBS Pro software is licensed under the terms of the GNU Affero General 
+# Public License agreement ("AGPL"), except where a separate commercial license 
+# agreement for PBS Pro version 14 or later has been executed in writing with Altair.
+# 
+# Altair’s dual-license business model allows companies, individuals, and 
+# organizations to create proprietary derivative works of PBS Pro and distribute 
+# them - whether embedded or bundled with other software - under a commercial 
+# license agreement.
+#
+# Use of Altair’s trademarks, including but not limited to "PBS™", 
+# "PBS Professional®", and "PBS Pro™" and Altair’s logos is subject to Altair's 
+# trademark licensing policies.
+
+from ptl.utils.pbs_testsuite import *
+
+
+class TestNodesQueues(PBSTestSuite):
+
+    def setUp(self):
+        PBSTestSuite.setUp(self)
+        self.server.add_resource('foo', 'string', 'h')
+
+        a = {'resources_available.ncpus': 4}
+        self.server.create_vnodes(
+            'vnode', a, 8, self.mom, attrfunc=self.cust_attr)
+
+        a = {'queue_type': 'execution', 'started': 't', 'enabled': 't'}
+        self.server.manager(MGR_CMD_CREATE, QUEUE, a, id='workq2')
+
+        self.server.manager(MGR_CMD_SET, NODE, {
+                            'queue': 'workq2'}, id='vnode[0]')
+        self.server.manager(MGR_CMD_SET, NODE, {
+                            'queue': 'workq2'}, id='vnode[1]')
+        self.server.manager(MGR_CMD_SET, NODE, {
+                            'queue': 'workq2'}, id='vnode[2]')
+        self.server.manager(MGR_CMD_SET, NODE, {
+                            'queue': 'workq2'}, id='vnode[3]')
+
+        self.server.manager(MGR_CMD_SET, SERVER, {'node_group_key': 'foo'})
+        self.server.manager(MGR_CMD_SET, SERVER, {'node_group_enable': 't'})
+
+    def cust_attr(self, name, totnodes, numnode, attrib):
+        a = {}
+        if numnode % 2 == 0:
+            a['resources_available.foo'] = 'A'
+        else:
+            a['resources_available.foo'] = 'B'
+        return dict(attrib.items() + a.items())
+
+    def tearDown(self):
+        PBSTestSuite.tearDown(self)
+        self.server.manager(MGR_CMD_UNSET, NODE, 'queue', id='vnode[0]')
+        self.server.manager(MGR_CMD_UNSET, NODE, 'queue', id='vnode[1]')
+        self.server.manager(MGR_CMD_UNSET, NODE, 'queue', id='vnode[2]')
+        self.server.manager(MGR_CMD_UNSET, NODE, 'queue', id='vnode[3]')
+
+    def test_node_queue_assoc_ignored(self):
+        """
+        Issue with node grouping and nodes associated with queues.  If
+        node_grouping is set at the server level, node/queue association is
+        not honored
+        """
+
+        a = {'Resource_List.select': '2:ncpus=1', 'Resource_List.place': 'vscatter', 'queue': 'workq2'}
+        j = Job(TEST_USER, attrs=a)
+        jid=self.server.submit(j)
+        self.server.expect(JOB, 'exec_vnode', id=jid, op=SET)
+        nodes = j.get_vnodes(j.exec_vnode)
+        self.assertTrue((nodes[0] == 'vnode[0]' and nodes[1] == 'vnode[2]') or (nodes[0] == 'vnode[1]' and nodes[1] == 'vnode[3]'))
