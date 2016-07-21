8d06e09f1a1747efcd64803d9e2adaf7f120a4ad
chitrac <chitra.chandel88@gmail.com>
2016-07-21 15:32:16 +0530

    PP-375 - Update swig on Window

--- win_configure/projects.VS2008/gen_ifl_wrap.bat
+++ win_configure/projects.VS2008/gen_ifl_wrap.bat
@@ -1,14 +1,14 @@
 @echo off
 REM this genereates the pbs_ifl_wrap.c file.
 
-if exist "C:\Program Files\swigwin-2.0.4\swig.exe" (
+if exist "C:\Program Files\swigwin-3.0.10\swig.exe" (
 	if not exist "pbs_ifl_wrap.c" (
 		echo %%module pbs_ifl > pbs_ifl.i
 		echo %%{ >> pbs_ifl.i
 		echo #include "..\..\src\include\pbs_ifl.h" >> pbs_ifl.i
 		echo %%} >> pbs_ifl.i
 		echo %%include "..\..\src\include\pbs_ifl.h" >> pbs_ifl.i
-		"C:\Program Files\swigwin-2.0.4\swig" -python pbs_ifl.i
+		"C:\Program Files\swigwin-3.0.10\swig" -python pbs_ifl.i
 	)
 ) else (
 	copy  ..\..\src\lib\Libifl\pbs_ifl_wrap.c .
