d46648d5febb0e886fa5561238ed4d62c38a3cce
Hiren Vadalia <hiren.vadalia@altair.com>
2017-02-09 10:35:04 +0530

    PP-664: Travis builds failing on centos

--- .travis/centos_7.sh
+++ .travis/centos_7.sh
@@ -1,4 +1,7 @@
 #!/bin/bash -xe
+${DOCKER_EXEC} /bin/bash -c "sed -i 's@baseurl=@#baseurl=@g' /etc/yum.repos.d/CentOS-Base.repo"
+${DOCKER_EXEC} /bin/bash -c "sed -i 's@#mirrorlist=@mirrorlist=@g' /etc/yum.repos.d/CentOS-Base.repo"
+${DOCKER_EXEC} /bin/bash -c "sed -i 's@\$releasever@7.2.1511@g' /etc/yum.repos.d/CentOS-Sources.repo"
 ${DOCKER_EXEC} yum -y update
 ${DOCKER_EXEC} yum -y install yum-utils epel-release rpmdevtools
 ${DOCKER_EXEC} yum -y install time.x86_64
@@ -12,3 +15,4 @@ ${DOCKER_EXEC} rpmbuild -bb pbspro.spec
 ${DOCKER_EXEC} /bin/sh -c 'yum -y install /root/rpmbuild/RPMS/x86_64/pbspro-server-*.x86_64.rpm'
 ${DOCKER_EXEC} /etc/init.d/pbs start
 ${DOCKER_EXEC} yum -y install python-pip sudo which net-tools
+
