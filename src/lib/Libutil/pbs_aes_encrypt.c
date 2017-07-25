ec16369b355d78ddb8a7cd2f8a214bee4b4530e8
Václav Chlumský <chlumskyvaclav@gmail.com>
2017-07-25 08:05:01 +0200

    PP-871: OpenSSL 1.1.0 compatibility

--- src/lib/Libutil/pbs_aes_encrypt.c
+++ src/lib/Libutil/pbs_aes_encrypt.c
@@ -33,9 +33,11 @@ extern unsigned char pbs_aes_iv[];
 int
 pbs_encrypt_data(char *uncrypted, int *credtype, size_t len, char **crypted, size_t *outlen)
 {
-        EVP_CIPHER_CTX ctx;
         int plen, len2 = 0;
         char *cblk;
+
+#if OPENSSL_VERSION_NUMBER < 0x10100000L
+        EVP_CIPHER_CTX ctx;
 
         EVP_CIPHER_CTX_init(&ctx);
 
@@ -54,7 +56,30 @@ pbs_encrypt_data(char *uncrypted, int *credtype, size_t len, char **crypted, siz
                 return -1;
 
         EVP_CIPHER_CTX_cleanup(&ctx);
+#else
+        EVP_CIPHER_CTX *ctx;
 
+        ctx = EVP_CIPHER_CTX_new();
+
+        if (ctx == NULL)
+                return -1;
+
+        if (EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, (const unsigned char *) pbs_aes_key, (const unsigned char *) pbs_aes_iv) == 0)
+                return -1;
+
+        plen = len + EVP_CIPHER_CTX_block_size(ctx) + 1;
+        cblk = malloc(plen);
+        if (!cblk)
+                return -1;
+
+        if (EVP_EncryptUpdate(ctx, cblk, &plen, uncrypted, len) == 0)
+                return -1;
+
+        if (EVP_EncryptFinal_ex(ctx, cblk + plen, &len2) == 0)
+                return -1;
+
+        EVP_CIPHER_CTX_free(ctx);
+#endif
         *crypted = cblk;
         *outlen = plen + len2;
         *credtype = PBS_CREDTYPE_AES;
@@ -90,10 +115,11 @@ pbs_encrypt_pwd(char *str, int *credtype, char **credbuf, size_t *credlen)
 int
 pbs_decrypt_data(char *crypted, int credtype, size_t len, char **uncrypted, size_t *outlen)
 {
-        EVP_CIPHER_CTX ctx;
         char *cblk;
         int plen, len2 = 0;
 
+#if OPENSSL_VERSION_NUMBER < 0x10100000L
+        EVP_CIPHER_CTX ctx;
 
         EVP_CIPHER_CTX_init(&ctx);
 
@@ -111,7 +137,29 @@ pbs_decrypt_data(char *crypted, int credtype, size_t len, char **uncrypted, size
                 return -1;
 
         EVP_CIPHER_CTX_cleanup(&ctx);
+#else
+        EVP_CIPHER_CTX *ctx;
 
+        ctx = EVP_CIPHER_CTX_new();
+
+        if (ctx == NULL)
+                return -1;
+
+        if (EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, (const unsigned char *) pbs_aes_key, (const unsigned char *) pbs_aes_iv) == 0)
+                return -1;
+
+        cblk = malloc(len + EVP_CIPHER_CTX_block_size(ctx) + 1);
+        if (!cblk)
+                return -1;
+
+        if (EVP_DecryptUpdate(ctx, cblk, &plen, crypted, len) == 0)
+                return -1;
+
+        if (EVP_DecryptFinal_ex(ctx, cblk + plen, &len2) == 0)
+                return -1;
+
+        EVP_CIPHER_CTX_free(ctx);
+#endif
         *uncrypted = cblk;
         *outlen = plen + len2;
 
