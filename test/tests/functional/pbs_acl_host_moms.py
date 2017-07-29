fa06c03c79db56c3f13b58a009d3bc05d3c5f004
Siddharth Sahay <sahaysid@gmail.com>
2017-07-29 15:58:51 +0530

    PP-765: Possibility to allow all moms in acl (Modified PTL test case)

--- test/tests/functional/pbs_acl_host_moms.py
+++ test/tests/functional/pbs_acl_host_moms.py
@@ -52,40 +52,44 @@ class Test_acl_host_moms(TestFunctional):
 
         TestFunctional.setUp(self)
 
-        if len(self.moms) != 2:
-            self.skip_test('test requires two MoMs as input, ' +
-                           '  use -p moms=<mom1>:<mom2>')
+        usage_string = 'test requires a MoM and a client as input, ' + \
+                       ' use -p moms=<mom>,client=<client>'
 
         # PBSTestSuite returns the moms passed in as parameters as dictionary
         # of hostname and MoM object
         self.momA = self.moms.values()[0]
-        self.momB = self.moms.values()[1]
         self.momA.delete_vnode_defs()
-        self.momB.delete_vnode_defs()
 
         self.hostA = self.momA.shortname
-        self.hostB = self.momB.shortname
+        if not self.du.is_localhost(self.server.client):
+            self.hostB = self.server.client
+        else:
+            self.skip_test(usage_string)
 
         self.remote_host = None
 
         if not self.du.is_localhost(self.hostA):
             self.remote_host = self.hostA
-
-        if not self.du.is_localhost(self.hostB):
-            self.remote_host = self.hostB
+        else:
+            self.skip_test(usage_string)
 
         self.assertTrue(self.remote_host)
 
         self.server.manager(MGR_CMD_SET, SERVER, {
+                            'acl_hosts': self.hostB}, expect=True)
+        self.server.manager(MGR_CMD_SET, SERVER, {
                             'acl_host_enable': True}, expect=True)
 
         self.pbsnodes_cmd = os.path.join(self.server.pbs_conf[
-            'PBS_EXEC'], 'bin', 'pbsnodes') + ' -a'
+            'PBS_EXEC'], 'bin', 'pbsnodes') + ' -av'
+
+        self.qstat_cmd = os.path.join(self.server.pbs_conf[
+            'PBS_EXEC'], 'bin', 'qstat')
 
     def test_acl_host_moms_enable(self):
         """
         Set acl_host_moms_enable = True and check whether or not the remote
-        host is able run pbsnodes.
+        host is able run pbsnodes and qstat.
         """
 
         self.server.manager(MGR_CMD_SET, SERVER, {
@@ -94,13 +98,90 @@ class Test_acl_host_moms(TestFunctional):
         ret = self.du.run_cmd(self.remote_host, cmd=self.pbsnodes_cmd)
         self.assertEqual(ret['rc'], 0)
 
+        ret = self.du.run_cmd(self.remote_host, cmd=self.qstat_cmd)
+        self.assertEqual(ret['rc'], 0)
+
     def test_acl_host_moms_disable(self):
         """
         Set acl_host_moms_enable = False and check whether or not the remote
-        host is forbidden to run pbsnodes.
+        host is forbidden to run pbsnodes and qstat.
         """
         self.server.manager(MGR_CMD_SET, SERVER, {
                             'acl_host_moms_enable': False}, expect=True)
 
         ret = self.du.run_cmd(self.remote_host, cmd=self.pbsnodes_cmd)
-        self.assertEqual(ret['rc'], 1)
+        self.assertNotEqual(ret['rc'], 0)
+
+        ret = self.du.run_cmd(self.remote_host, cmd=self.qstat_cmd)
+        self.assertNotEqual(ret['rc'], 0)
+
+    def test_acl_host_moms_hooks_and_jobs(self):
+        """
+        Use hooks to test whether remote host is able to run pbs.server()
+        and check whether the job that is submitted goes to the 'R' state.
+        """
+        hook_name = "hook_acl_host_moms_t"
+        hook_body = """
+import pbs
+e = pbs.event()
+svr = pbs.server().server_state
+e.accept()
+"""
+        try:
+            self.server.manager(MGR_CMD_DELETE, HOOK, None, hook_name)
+        except Exception:
+            pass
+
+        a = {'event': 'execjob_begin', 'enabled': 'True'}
+        self.server.create_import_hook(
+            hook_name, a, hook_body, overwrite=True)
+
+        self.server.manager(MGR_CMD_SET, SERVER, {
+                            'acl_host_moms_enable': False}, expect=True)
+
+        j = Job()
+        j.set_sleep_time(10)
+        jid = self.server.submit(j)
+
+        self.server.expect(JOB, {'job_state': 'H'}, id=jid)
+
+        self.server.manager(MGR_CMD_SET, SERVER, {
+                            'acl_host_moms_enable': True}, expect=True)
+        j = Job()
+        j.set_sleep_time(10)
+        jid = self.server.submit(j)
+
+        self.server.expect(JOB, {'job_state': 'R'}, id=jid)
+
+    def test_acl_host_mom_queue_access(self):
+        """
+        Test that remote host cannot submit jobs to queue where
+        acl_host_enable is True and acl_host_moms_enable is set
+        on server, but remote host is not added in acl_hosts.
+        """
+        queue_n = 'tempq'
+        queue_params = {'queue_type': 'Execution', 'enabled': 'True',
+                        'started': 'True', 'acl_host_enable': 'True'}
+        self.server.manager(MGR_CMD_CREATE, QUEUE, queue_params, id='tempq')
+
+        self.server.manager(MGR_CMD_SET, SERVER, {
+                            'acl_host_moms_enable': True}, expect=True)
+        # Setting acl_host_enable on queue overrides acl_host_moms_enable
+        # on server and requires acl_hosts to include remote host's name.
+
+        self.server.manager(MGR_CMD_SET, SERVER, {
+                            'flatuid': True}, expect=True)
+        # Setting flatuid lets us submit jobs on server as a remote
+        # host without creating a seperate user account there.
+        qsub_cmd_on_queue = os.path.join(self.server.pbs_conf[
+            'PBS_EXEC'], 'bin',
+            'qsub') + ' -q ' + queue_n + ' -- /bin/sleep 10'
+
+        j = Job(attrs={ATTR_queue: queue_n})
+        j.set_sleep_time(10)
+        cannot_submit = 0
+        try:
+            jid = self.server.submit(j)
+        except PbsSubmitError:
+            cannot_submit = 1
+        self.assertEqual(cannot_submit, 1)
