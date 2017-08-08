97326aae09ebcd1ec21cdeb22a1f18d70c3cabe7
Anamika <anamika@altair.com>
2017-08-07 17:38:19 -0700

    PP-718: Added more regressions for fairshare formula

--- test/tests/functional/pbs_fairshare.py
+++ test/tests/functional/pbs_fairshare.py
@@ -41,6 +41,7 @@ from tests.functional import *
 
 @tags('sched')
 class TestFairshare(TestFunctional):
+
     """
     Test the pbs_sched fairshare functionality.  Note there are two
     fairshare tests in the standard smoke test that are not included here.
@@ -78,6 +79,13 @@ class TestFairshare(TestFunctional):
 
         formula = 'fairshare_factor'
         self.server.manager(MGR_CMD_SET, SERVER, {'job_sort_formula': formula})
+
+        formula = 'fair_share_factor'
+        try:
+            self.server.manager(
+                MGR_CMD_SET, SERVER, {'job_sort_formula': formula})
+        except PbsManagerError as e:
+            self.assertTrue("Formula contains invalid keyword" in e.msg[0])
 
     def test_fairshare_formula(self):
         """
@@ -184,6 +192,7 @@ class TestFairshare(TestFunctional):
 
         self.server.manager(MGR_CMD_SET, SERVER, {'scheduling': 'False'})
         self.server.manager(MGR_CMD_SET, SERVER, {'job_sort_formula': formula})
+
         J1 = Job(TEST_USER2)
         jid1 = self.server.submit(J1)
         J2 = Job(TEST_USER3)
@@ -203,6 +212,86 @@ class TestFairshare(TestFunctional):
 
         c = self.scheduler.cycles(lastN=1)[0]
         job_order = [jid3, jid4, jid1, jid2]
+        for i in range(len(job_order)):
+            self.assertEqual(job_order[i].split('.')[0], c.political_order[i])
+
+    def test_fairshare_formula5(self):
+        """
+        Test fairshare in the formula with fair_share set to true in scheduler.
+        Make sure formula takes precedence over fairshare usage. Output will be
+        same as in test_fairshare_formula4.
+        """
+
+        self.set_up_resource_group()
+        a = {'log_filter': 2048, 'fair_share': "True All"}
+        self.scheduler.set_sched_config(a)
+
+        formula = 'fairshare_factor'
+
+        self.server.manager(MGR_CMD_SET, SERVER, {'scheduling': 'False'})
+        self.server.manager(MGR_CMD_SET, SERVER, {'job_sort_formula': formula})
+
+        J1 = Job(TEST_USER2, {'Resource_List.cput': 10})
+        jid1 = self.server.submit(J1)
+        J2 = Job(TEST_USER3, {'Resource_List.cput': 20})
+        jid2 = self.server.submit(J2)
+        J3 = Job(TEST_USER, {'Resource_List.cput': 30})
+        jid3 = self.server.submit(J3)
+        J4 = Job(TEST_USER1, {'Resource_List.cput': 40})
+        jid4 = self.server.submit(J4)
+        t = int(time.time())
+        self.server.manager(MGR_CMD_SET, SERVER, {'scheduling': 'True'})
+        msg = ';Formula Evaluation = '
+        self.scheduler.log_match(str(jid1) + msg + '0.3816', max_attempts=2)
+        self.scheduler.log_match(str(jid2) + msg + '0.0902', max_attempts=2)
+        self.scheduler.log_match(str(jid3) + msg + '0.6477', max_attempts=2)
+        self.scheduler.log_match(str(jid4) + msg + '0.6477', max_attempts=2)
+        self.scheduler.log_match('Leaving Scheduling Cycle', starttime=t)
+
+        c = self.scheduler.cycles(start=t, lastN=1)[0]
+        job_order = [jid3, jid4, jid1, jid2]
+        for i in range(len(job_order)):
+            self.assertEqual(job_order[i].split('.')[0], c.political_order[i])
+
+    def test_fairshare_formula6(self):
+        """
+        Test fairshare in the formula.  Make sure entities with small usage
+        are negatively affected by their high usage siblings.  Make sure that
+        jobs run in the correct order.  Use keyword fairshare_factor
+        with ncpus/walltime
+        """
+
+        self.set_up_resource_group()
+        self.scheduler.set_sched_config({'log_filter': 2048})
+
+        formula = '\"fairshare_factor + (walltime/ncpus)\"'
+
+        self.server.manager(MGR_CMD_SET, SERVER, {'scheduling': 'False'})
+        self.server.manager(MGR_CMD_SET, SERVER, {'job_sort_formula': formula})
+
+        J1 = Job(TEST_USER2, {'Resource_List.ncpus': 1,
+                              'Resource_List.walltime': "00:01:00"})
+        jid1 = self.server.submit(J1)
+        J2 = Job(TEST_USER3, {'Resource_List.ncpus': 2,
+                              'Resource_List.walltime': "00:01:00"})
+        jid2 = self.server.submit(J2)
+        J3 = Job(TEST_USER, {'Resource_List.ncpus': 3,
+                             'Resource_List.walltime': "00:02:00"})
+        jid3 = self.server.submit(J3)
+        J4 = Job(TEST_USER1, {'Resource_List.ncpus': 4,
+                              'Resource_List.walltime': "00:02:00"})
+        jid4 = self.server.submit(J4)
+        t = int(time.time())
+        self.server.manager(MGR_CMD_SET, SERVER, {'scheduling': 'True'})
+        msg = ';Formula Evaluation = '
+        self.scheduler.log_match(str(jid1) + msg + '60.3816', max_attempts=2)
+        self.scheduler.log_match(str(jid2) + msg + '30.0902', max_attempts=2)
+        self.scheduler.log_match(str(jid3) + msg + '40.6477', max_attempts=2)
+        self.scheduler.log_match(str(jid4) + msg + '30.6477', max_attempts=2)
+        self.scheduler.log_match('Leaving Scheduling Cycle', starttime=t)
+
+        c = self.scheduler.cycles(start=t, lastN=1)[0]
+        job_order = [jid1, jid3, jid4, jid2]
         for i in range(len(job_order)):
             self.assertEqual(job_order[i].split('.')[0], c.political_order[i])
 
