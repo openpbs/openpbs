3247d850e3d03f5d908686bc68ae2f67d9847414
Hiren Vadalia <hiren.vadalia@altair.com>
2017-10-13 09:04:41 +0530

    PP-1029: Travis needs to be updated to install man command

--- .travis/centos_7.sh
+++ .travis/centos_7.sh
@@ -4,7 +4,6 @@ ${DOCKER_EXEC} /bin/bash -c "sed -i 's@#mirrorlist=@mirrorlist=@g' /etc/yum.repo
 ${DOCKER_EXEC} /bin/bash -c "sed -i 's@\$releasever@7.2.1511@g' /etc/yum.repos.d/CentOS-Sources.repo"
 ${DOCKER_EXEC} yum -y update
 ${DOCKER_EXEC} yum -y install yum-utils epel-release rpmdevtools
-${DOCKER_EXEC} yum -y install time.x86_64
 ${DOCKER_EXEC} rpmdev-setuptree
 ${DOCKER_EXEC} yum-builddep -y ./pbspro.spec
 ${DOCKER_EXEC} ./autogen.sh
@@ -14,5 +13,5 @@ ${DOCKER_EXEC} /bin/sh -c 'cp -fv pbspro-*.tar.gz /root/rpmbuild/SOURCES/'
 ${DOCKER_EXEC} rpmbuild -bb pbspro.spec
 ${DOCKER_EXEC} /bin/sh -c 'yum -y install /root/rpmbuild/RPMS/x86_64/pbspro-server-*.x86_64.rpm'
 ${DOCKER_EXEC} /etc/init.d/pbs start
-${DOCKER_EXEC} yum -y install python-pip sudo which net-tools
+${DOCKER_EXEC} yum -y install python-pip sudo which net-tools man-db time.x86_64
 
--- .travis/opensuse.sh
+++ .travis/opensuse.sh
@@ -3,7 +3,6 @@ OS_NAME=$(${DOCKER_EXEC} cat /etc/os-release | awk -F[=\"] '/^NAME=/ {print $3}'
 OS_VERSION=$(${DOCKER_EXEC} cat /etc/os-release | awk -F[=\"] '/VERSION=/ {print $3}')
 OS_NAME_VERSION=${OS_NAME// /_}_${OS_VERSION// /_}
 ${DOCKER_EXEC} zypper -n ar -f -G http://download.opensuse.org/repositories/devel:/tools/${OS_NAME_VERSION}/devel:tools.repo
-${DOCKER_EXEC} zypper -n install time.x86_64
 ${DOCKER_EXEC} zypper -n ref
 ${DOCKER_EXEC} zypper -n install rpmdevtools
 ${DOCKER_EXEC} rpmdev-setuptree
@@ -15,4 +14,5 @@ ${DOCKER_EXEC} /bin/sh -c 'cp -fv pbspro-*.tar.gz /root/rpmbuild/SOURCES/'
 ${DOCKER_EXEC} rpmbuild -bb pbspro.spec
 ${DOCKER_EXEC} /bin/sh -c 'zypper --no-gpg-checks -n install /root/rpmbuild/RPMS/x86_64/pbspro-server-*.x86_64.rpm'
 ${DOCKER_EXEC} /etc/init.d/pbs start
-${DOCKER_EXEC} zypper -n install python-pip sudo which net-tools
+${DOCKER_EXEC} zypper -n install python-pip sudo which net-tools man time.x86_64
+
