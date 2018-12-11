3414c3a81c778fe37b86e6724dc7e494149b32cb
Shrinivas Harapanahalli <shrinivas.harapanahalli@altair.com>
2018-12-11 14:22:03 +0530

    test_queue_deletion_after_terminated_subjob fails if server restart is slow

--- test/tests/functional/pbs_job_array.py
+++ test/tests/functional/pbs_job_array.py
@@ -414,7 +414,6 @@ class TestJobArray(TestFunctional):
         self.server.manager(MGR_CMD_SET, NODE, a, self.mom.shortname)
         j = Job(TEST_USER, attrs={
             ATTR_J: '1-2', 'Resource_List.select': 'ncpus=1'})
-        j.set_sleep_time(5)
         j_id = self.server.submit(j)
         subjid_1 = j.create_subjob_id(j_id, 1)
         a = {'job_state': 'R', 'run_count': 1}
