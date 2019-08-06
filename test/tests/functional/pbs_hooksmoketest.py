246b0b87b46a759392dc9d32fd58330a08b29b61
Lovely Kumari <lovely.kumari@altair.com>
2019-08-06 12:15:23 +0530

    Addressed Prakash's review comments

--- test/tests/functional/pbs_hooksmoketest.py
+++ test/tests/functional/pbs_hooksmoketest.py
@@ -19,25 +19,21 @@
 from tests.functional import *
 
 
-@tags('hooks')
+@tags('hooks', 'smoke')
 class TestHookSmokeTest(TestFunctional):
-
     """
     Hooks Smoke Test
     """
-
     def setUp(self):
         TestFunctional.setUp(self)
-        self.server.manager(MGR_CMD_SET, SERVER, {'log_events': 2047})
-        self.server.manager(MGR_CMD_SET, SERVER,
-                            {'scheduling': 'False'})
+        a = {'log_events': 2047, 'scheduling': 'False'}
+        self.server.manager(MGR_CMD_SET, SERVER, a)
         self.clean_hooks()
 
-    def get_file_path(self, hook_name):
+    def get_file_paths(self, hook_name, existence=False):
         """
         This function returns the path of server's hook
-        directory, name of the py file, and path of the PY file
-        and HK file
+        directory, name of the .HK file, and path of HK file
         """
         conf = self.du.parse_pbs_config()
         pbs_home = conf['PBS_HOME']
@@ -49,25 +45,24 @@ class TestHookSmokeTest(TestFunctional):
         self.logger.info("As expected dir '%s' present" % server_hooks_dir)
         hk_file = hook_name + ".HK"
         hk_file_location = os.path.join(server_hooks_dir, hk_file)
-        return server_hooks_dir, hk_file, hk_file_location
-
-    def check_hk_file(self, existence):
+        self.logger.info("Check existence of .HK file")
+        count = 2
         if existence:
-            msg = "As expected file '%s' is present" % self.hk_file
-            msg += " in '%s' directory" % self.server_hooks_dir
-            _msg = "File '%s' is not present" % self.hk_file
-            _msg += " in '%s' directory" % self.server_hooks_dir
+            count = 10
+            msg = "As expected file '%s' is present" % hk_file
+            msg += " in '%s' directory" % server_hooks_dir
+            _msg = "File '%s' is not present" % hk_file
+            _msg += " in '%s' directory" % server_hooks_dir
         else:
-            msg = "As expected file '%s' is not present" % self.hk_file
-            msg += " in '%s' directory" % self.server_hooks_dir
-            _msg = "File '%s' is present" % self.hk_file
-            _msg += " in '%s' directory" % self.server_hooks_dir
+            msg = "As expected file '%s' is not present" % hk_file
+            msg += " in '%s' directory" % server_hooks_dir
+            _msg = "File '%s' is present" % hk_file
+            _msg += " in '%s' directory" % server_hooks_dir
 
         # sleeping for some time as generation of *.HK file takes time
-        count = 5
         while True:
             rc = self.du.isfile(hostname=self.server.hostname,
-                                path=self.hk_file_location,
+                                path=hk_file_location,
                                 sudo=True)
             count = count - 1
             if rc or count == 0:
@@ -75,6 +70,7 @@ class TestHookSmokeTest(TestFunctional):
             time.sleep(1)
         self.assertEqual(rc, existence, _msg)
         self.logger.info(msg)
+        return server_hooks_dir, hk_file, hk_file_location
 
     def clean_hooks(self):
         """
@@ -84,31 +80,30 @@ class TestHookSmokeTest(TestFunctional):
             self.server.manager(MGR_CMD_LIST, HOOK)
         except:
             pass
-        if 'my_hook_queuejob' in self.server.hooks:
-            self.server.manager(MGR_CMD_DELETE, HOOK, id="my_hook_queuejob")
-        if 'my_hook_queuejob2' in self.server.hooks:
-            self.server.manager(MGR_CMD_DELETE, HOOK, id="my_hook_queuejob2")
-        if 'my_hook' in self.server.hooks:
-            self.server.manager(MGR_CMD_DELETE, HOOK, id="my_hook")
+        del_hook = ['test_hook_queuejob',
+                    'test_hook_queuejob2', 'test_hook2', 'test_hook']
+        for i in del_hook:
+            if i in self.server.hooks:
+                self.server.manager(MGR_CMD_DELETE, HOOK, id=i)
 
     def test_create_and_print_hook(self):
         """
         Test create and print a hook
         """
-        hook_name = "my_hook_queuejob"
+        hook_name = "test_hook_queuejob"
         attrs = {'event': 'queuejob'}
-        self.logger.info('Create hook my_hook_queuejob')
+        self.logger.info('Create hook test_hook_queuejob')
         self.server.create_hook(hook_name, attrs)
-        self.logger.info(
-            'Verify .HK file hook exists in PBS_HOME/server_priv/hooks')
         (self.server_hooks_dir, self.hk_file,
-         self.hk_file_location) = self.get_file_path(hook_name)
-        self.check_hk_file(existence=True)
+         self.hk_file_location) = self.get_file_paths(hook_name,
+                                                      existence=True)
+        self.logger.info('Verified ' + self.hk_file + ' file hook exists in' +
+                         self.server_hooks_dir + ' as expected')
 
         attrs = {'type': 'site', 'enabled': 'true', 'event': 'queuejob',
                  'alarm': 30, 'order': 1, 'debug': 'false',
                  'user': 'pbsadmin', 'fail_action': 'none'}
-        self.logger.info('Verify hook values for my_hook_queuejob')
+        self.logger.info('Verify hook values for test_hook_queuejob')
         rc = self.server.manager(MGR_CMD_LIST, HOOK,
                                  id=hook_name)
         self.assertEqual(rc, 0)
@@ -119,28 +114,23 @@ class TestHookSmokeTest(TestFunctional):
         """
         Test import and export hook
         """
-        hook_name = "my_hook_queuejob2"
-        hook_body = """
-import pbs
-
+        hook_name = "test_hook_queuejob2"
+        hook_body = """import pbs
 e = pbs.event()
 j = e.job
-
 if j.Resource_List["walltime"] is None:
   e.reject("no walltime specified")
-
 j.Resource_List["mem"] = pbs.size("7mb")
-e.accept()
-"""
+e.accept()"""
         imp_hook_body = hook_body.split('\n')
-        exp_hook_body = imp_hook_body[0:len(imp_hook_body) - 1]
+        exp_hook_body = imp_hook_body
         attrs = {'event': 'queuejob'}
         self.server.create_import_hook(hook_name, attrs, hook_body)
         fn = self.du.create_temp_file()
         hook_attrs = 'application/x-config default %s' % fn
         rc = self.server.manager(MGR_CMD_EXPORT, HOOK, hook_attrs, hook_name)
         self.assertEqual(rc, 0)
-        cmd = "export h my_hook_queuejob2 application/x-python default"
+        cmd = "export h test_hook_queuejob2 application/x-python default"
         export_cmd = [os.path.join(self.server.pbs_conf['PBS_EXEC'], 'bin',
                                    'qmgr'), '-c', cmd]
         ret = self.du.run_cmd(self.server.hostname, export_cmd, sudo=True)
@@ -151,21 +141,21 @@ e.accept()
         """
         Test enable and disable a hook
         """
-        hook_name = "my_hook"
+        hook_name = "test_hook"
         rc = self.server.manager(MGR_CMD_CREATE, HOOK, None, hook_name)
         self.assertEqual(rc, 0)
         self.server.manager(MGR_CMD_SET, HOOK, {'enabled': 0}, id=hook_name)
         attrs = {'type': 'site', 'enabled': 'false', 'event': '""',
                  'alarm': 30, 'order': 1, 'debug': 'false',
                  'user': 'pbsadmin', 'fail_action': 'none'}
-        self.logger.info('Verify hook values for my_hook')
+        self.logger.info('Verify hook values for test_hook')
         rc = self.server.manager(MGR_CMD_LIST, HOOK,
                                  id=hook_name)
         self.assertEqual(rc, 0)
         rv = self.server.expect(HOOK, attrs, id=hook_name)
         self.assertTrue(rv)
         self.server.manager(MGR_CMD_SET, HOOK, {'enabled': 1}, id=hook_name)
-        self.logger.info('Verify hook values for my_hook')
+        self.logger.info('Verify hook values for test_hook')
         attrs = {'type': 'site', 'enabled': 'true', 'event': '""',
                  'alarm': 30, 'order': 1, 'debug': 'false',
                  'user': 'pbsadmin', 'fail_action': 'none'}
@@ -179,10 +169,10 @@ e.accept()
         """
         Test to modify a hook"
         """
-        hook_name = "my_hook2"
+        hook_name = "test_hook2"
         attrs = {'event': 'queuejob', 'alarm': 60,
                  'enabled': 'false', 'order': 7}
-        self.logger.info('Create hook my_hook2')
+        self.logger.info('Create hook test_hook2')
         rv = self.server.create_hook(hook_name, attrs)
         self.assertTrue(rv)
         rc = self.server.manager(MGR_CMD_LIST, HOOK,
@@ -190,10 +180,10 @@ e.accept()
         self.assertEqual(rc, 0)
         rv = self.server.expect(HOOK, attrs, id=hook_name)
         self.assertTrue(rv)
-        self.logger.info("Modify hook my_hook2 event")
+        self.logger.info("Modify hook test_hook2 event")
         self.server.manager(MGR_CMD_SET, HOOK, {
                             'event+': 'resvsub'}, id=hook_name)
-        self.logger.info('Verify hook values for my_hook2')
+        self.logger.info('Verify hook values for test_hook2')
         attrs2 = {'event': 'queuejob,resvsub',
                   'alarm': 60, 'enabled': 'false', 'order': 7}
         rc = self.server.manager(MGR_CMD_LIST, HOOK,
@@ -203,7 +193,7 @@ e.accept()
         self.assertTrue(rv)
         self.server.manager(MGR_CMD_SET, HOOK, {
                             'event-': 'resvsub'}, id=hook_name)
-        self.logger.info('Verify hook values for my_hook2')
+        self.logger.info('Verify hook values for test_hook2')
         rc = self.server.manager(MGR_CMD_LIST, HOOK,
                                  id=hook_name)
         self.assertEqual(rc, 0)
@@ -214,25 +204,24 @@ e.accept()
         """
         Test delete a hook
         """
-        hook_name = "my_hook"
+        hook_name = "test_hook"
         rc = self.server.manager(MGR_CMD_CREATE, HOOK, None, hook_name)
         self.assertEqual(rc, 0)
         self.server.manager(MGR_CMD_DELETE, HOOK, id=hook_name)
-        cmd = "l h my_hook"
+        cmd = "l h test_hook"
         export_cmd = [os.path.join(self.server.pbs_conf['PBS_EXEC'], 'bin',
                                    'qmgr'), '-c', cmd]
         ret = self.du.run_cmd(self.server.hostname, export_cmd, sudo=True)
         err_msg = []
-        err_msg.append("qmgr obj=my_hook svr=default: hook not found")
+        err_msg.append("qmgr obj=test_hook svr=default: hook not found")
         err_msg.append("qmgr: hook error returned from server")
         for i in err_msg:
             self.assertIn(i, ret['err'],
                           msg="Failed to get expected error message")
-        self.logger.info(
-            'Verify .HK file doesnot exists in PBS_HOME/server_priv/hooks')
         (self.server_hooks_dir, self.hk_file,
-         self.hk_file_location) = self.get_file_path(hook_name)
-        self.check_hk_file(existence=False)
+         self.hk_file_location) = self.get_file_paths(hook_name)
+        self.logger.info('Verified ' + self.hk_file + ' file not exists in' +
+                         self.server_hooks_dir + ' as expected')
 
     def tearDown(self):
         self.clean_hooks()
