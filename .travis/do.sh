933c6fc847380962ca45a8c24e2c4ef541968afd
Hiren Vadalia <hiren.vadalia@altair.com>
2019-08-21 18:36:57 +0530

    Use make && make install method in Travis

---
+++ .travis/do.sh
@@ -0,0 +1,91 @@
+#!/bin/bash -xe
+
+. /etc/os-release
+
+if [ "x${ONLY_REBUILD}" != "x1" ]; then
+  SPEC_FILE=$(dirname $(dirname $0))/pbspro.spec
+  if [ "x${ID}" == "xcentos" ]; then
+    yum clean all
+    yum -y update
+    yum -y install yum-utils epel-release rpmdevtools
+    yum -y install python-pip sudo which net-tools man-db time.x86_64 \
+                    expat libedit postgresql-server postgresql-contrib python \
+                    sendmail sudo tcl tk libical libasan llvm
+    rpmdev-setuptree
+    yum-builddep -y ${SPEC_FILE}
+    yum -y install $(rpmspec --requires -q ${SPEC_FILE} | awk '{print $1}'| sort -u | grep -vE '^(/bin/)?(ba)?sh$')
+    pip install -r test/fw/requirements.txt
+  elif [ "x${ID}" == "xopensuse" -o "x${ID}" == "xopensuse-leap" ]; then
+    _PRETTY_NAME=$(echo ${PRETTY_NAME} | awk -F[=\"] '{print $1}')
+    _PRETTY_NAME=${_PRETTY_NAME# }
+    _PRETTY_NAME=${_PRETTY_NAME% }
+    _PRETTY_NAME=${_PRETTY_NAME// /_}
+    _base_link="http://download.opensuse.org/repositories"
+    zypper -n ar -f -G ${_base_link}/devel:/tools/${_PRETTY_NAME}/devel:tools.repo
+    zypper -n ar -f -G ${_base_link}/devel:/libraries:/c_c++/${_PRETTY_NAME}/devel:libraries:c_c++.repo
+    zypper -n ref
+    zypper -n update
+    zypper -n install rpmdevtools python-pip sudo which net-tools man time.x86_64
+    rpmdev-setuptree
+    zypper -n install --force-resolution $(rpmspec --buildrequires -q ${SPEC_FILE} | awk '{print $1}'| sort -u | grep -vE '^(/bin/)?(ba)?sh$')
+    zypper -n install --force-resolution $(rpmspec --requires -q ${SPEC_FILE} | awk '{print $1}'| sort -u | grep -vE '^(/bin/)?(ba)?sh$')
+    pip install -r test/fw/requirements.txt
+  elif [ "x${ID}" == "xdebian" ]; then
+    if [ "x${DEBIAN_FRONTEND}" == "x" ]; then
+      export DEBIAN_FRONTEND=noninteractive
+    fi
+    apt-get -y update
+    apt-get -y upgrade
+    apt-get install -y build-essential dpkg-dev autoconf libtool rpm alien libssl-dev \
+                        libxt-dev libpq-dev libexpat1-dev libedit-dev libncurses5-dev \
+                        libical-dev libhwloc-dev pkg-config tcl-dev tk-dev python-dev \
+                        swig expat postgresql postgresql-contrib python-pip sudo man-db
+    pip install -r test/fw/requirements.txt
+  elif [ "x${ID}" == "xubuntu" ]; then
+    if [ "x${DEBIAN_FRONTEND}" == "x" ]; then
+      export DEBIAN_FRONTEND=noninteractive
+    fi
+    apt-get -y update
+    apt-get -y upgrade
+    apt-get install -y build-essential dpkg-dev autoconf libtool rpm alien libssl-dev \
+                        libxt-dev libpq-dev libexpat1-dev libedit-dev libncurses5-dev \
+                        libical-dev libhwloc-dev pkg-config tcl-dev tk-dev python-dev \
+                        swig expat postgresql python-pip sudo man-db
+    pip install -r test/fw/requirements.txt
+  else
+    echo "Unknown platform..."
+    exit 1
+  fi
+fi
+
+if [ "x${ONLY_INSTALL_DEPS}" == "x1" ]; then
+  exit 0
+fi
+
+_targetdirname=target-${ID}
+if [ "x${ONLY_REBUILD}" != "x1" ]; then
+  rm -rf ${_targetdirname}
+fi
+mkdir -p ${_targetdirname}
+if [ "x${ONLY_REBUILD}" != "x1" ]; then
+  [[ -f Makefile ]] && make distclean || true
+  ./autogen.sh
+  _cflags="-g -O2 -Wall -Werror"
+  if [ "x${ID}" == "xubuntu" ]; then
+    _cflags="${_cflags} -Wno-unused-result"
+  fi
+  cd ${_targetdirname}
+  ../configure CFLAGS="${_cflags}" --prefix /opt/pbs --enable-ptl
+  cd -
+fi
+cd ${_targetdirname}
+make -j8
+make -j8 install
+if [ "x${DONT_START_PBS}" != "x1" ]; then
+  chmod 4755 /opt/pbs/sbin/pbs_iff /opt/pbs/sbin/pbs_rcp
+  if [ "x${ONLY_REBUILD}" != "x1" ]; then
+    /opt/pbs/libexec/pbs_postinstall server
+    sed -i "s@PBS_START_MOM=0@PBS_START_MOM=1@" /etc/pbs.conf
+  fi
+  /etc/init.d/pbs restart
+fi
---
+++ .travis/do_sanitize_mode.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -xe
+
+yum clean all
+yum -y update
+yum -y install yum-utils epel-release rpmdevtools libasan llvm
+rpmdev-setuptree
+yum -y install python-pip sudo which net-tools man-db time.x86_64
+yum-builddep -y ./pbspro.spec
+./autogen.sh
+rm -rf target-sanitize
+mkdir -p target-sanitize
+cd target-sanitize
+../configure
+make dist
+cp -fv pbspro-*.tar.gz /root/rpmbuild/SOURCES/
+CFLAGS="-g -O2 -Wall -Werror -fsanitize=address -fno-omit-frame-pointer" rpmbuild -bb --with ptl pbspro.spec
+yum -y install /root/rpmbuild/RPMS/x86_64/pbspro-server-??.*.x86_64.rpm
+yum -y install /root/rpmbuild/RPMS/x86_64/pbspro-ptl-??.*.x86_64.rpm
+sed -i "s@PBS_START_MOM=0@PBS_START_MOM=1@" /etc/pbs.conf
+/etc/init.d/pbs start
