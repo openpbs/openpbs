f9d1295fef190b746b6ac4ab86505e2276f8314a
Václav Chlumský <chlumskyvaclav@gmail.com>
2019-11-13 15:52:32 +0100

    cached credentials not used with kerberos (#1397)

--- src/server/req_cred.c
+++ src/server/req_cred.c
@@ -183,8 +183,8 @@ get_cached_cred(job  *pjob)
 		return NULL;
 	}
 
-	strncpy(cred->credid, pjob->ji_wattr[(int)JOB_ATR_cred_id].at_val.at_str,
-		strlen(pjob->ji_wattr[(int)JOB_ATR_cred_id].at_val.at_str));
+	strncpy(cred->credid, pjob->ji_wattr[(int)JOB_ATR_cred_id].at_val.at_str, PBS_MAXUSER);
+	cred->credid[PBS_MAXUSER] = '\0';
 	cred->type = cred_type;
 	cred->validity = validity;
 	cred->size = strlen(buf);
