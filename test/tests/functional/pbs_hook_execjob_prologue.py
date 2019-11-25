7bcbff260b2b8eb2a22a462be155a9d9a3c2f877
sandisamp <sandisamp@gmail.com>
2019-11-25 12:39:40 +0530

    adding missing '-'

--- test/tests/functional/pbs_hook_execjob_prologue.py
+++ test/tests/functional/pbs_hook_execjob_prologue.py
@@ -261,7 +261,7 @@ class TestPbsExecutePrologue(TestFunctional):
         ret = self.du.cat(hostname=host, filename=opath, runas=TEST_USER)
         _msg = "cat command failed with error: %s" % ret['err']
         self.assertEqual(ret['rc'], 0, _msg)
-        ret['out']=ret['out'][2:]
+        ret['out']=ret['out'][-2:]
         mom1 = ret['out'][0].split(".")[0]
         mom2 = ret['out'][1].split(".")[0]
         self.exec_mom1 = self.moms[mom1]
