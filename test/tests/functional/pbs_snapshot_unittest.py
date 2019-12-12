b5737c880903314a72947981bed156191f413534
Ravi Agrawal <ragrawal@altair.com>
2019-12-11 20:04:02 -0500

    pbs_snapshot fails because file command not found (#1438)

--- test/tests/functional/pbs_snapshot_unittest.py
+++ test/tests/functional/pbs_snapshot_unittest.py
@@ -934,12 +934,11 @@ pbs.logmsg(pbs.EVENT_DEBUG,"%s")
                     if not fpath.startswith(sched_priv_dir):
                         self.fail("Unexpected file " + fpath + " captured")
 
-    @classmethod
-    def tearDownClass(self):
+    def tearDown(self):
         # Delete the snapshot directories and tarballs created
         for snap_dir in self.snapdirs:
             self.du.rm(path=snap_dir, recursive=True, force=True)
         for snap_tar in self.snaptars:
             self.du.rm(path=snap_tar, sudo=True, force=True)
 
-        TestFunctional.tearDownClass()
+        TestFunctional.tearDown(self)
