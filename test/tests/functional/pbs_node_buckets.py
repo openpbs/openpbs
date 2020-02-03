3be007ae3ceaf3efed646524774d0a8f4a64330f
birbalsain <sainbirbal161996@gmail.com>
2020-02-03 01:56:46 -0500

    use "mom.add_checkpoint_abort_script" to create checkpoint_abort_script

--- test/tests/functional/pbs_node_buckets.py
+++ test/tests/functional/pbs_node_buckets.py
@@ -436,11 +436,7 @@ class TestNodeBuckets(TestFunctional):
                 kill $1
                 exit 0
                 """
-        self.chk_file = self.du.create_temp_file(body=chk_script)
-        self.du.chmod(path=self.chk_file, mode=0o755)
-        self.du.chown(path=self.chk_file, uid=0, gid=0, sudo=True)
-        c = {'$action': 'checkpoint_abort 30 !' + self.chk_file + ' %sid'}
-        self.mom.add_config(c)
+        self.mom.add_checkpoint_abort_script(body=chk_script)
 
         self.server.manager(MGR_CMD_SET, SCHED, {'preempt_order': 'C'},
                             runas=ROOT_USER)
