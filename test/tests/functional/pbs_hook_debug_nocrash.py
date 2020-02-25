de447ff7bd6b2c7d7f4027998b08b95270eb11e5
Shilpa Kodli <shilpa.kodli@altair.com>
2020-02-25 13:00:05 +0530

    TestHookDebugNoCrash is failing due to timeout not being handled

--- test/tests/functional/pbs_hook_debug_nocrash.py
+++ test/tests/functional/pbs_hook_debug_nocrash.py
@@ -82,7 +82,7 @@ class TestHookDebugNoCrash(TestFunctional):
             self.skipTest(msg)
         TestFunctional.setUp(self)
 
-    @timeout(400)
+    @timeout(900)
     def test_hook_debug_no_crash(self):
 
         hook_body = """
