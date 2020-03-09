0f7465b93f3dbcff692825a5ce4cd5fa72c216d1
sandisamp <sandisamp@gmail.com>
2020-03-09 11:59:25 +0530

    test_attribute_case.TestExpect failed while verifying queue attribute

--- test/fw/ptl/lib/pbs_testlib.py
+++ test/fw/ptl/lib/pbs_testlib.py
@@ -1780,7 +1780,8 @@ class BatchUtils(object):
                 m = self.pbsobjattrval_re.match(l)
                 if m:
                     attr = m.group('attribute')
-                    if attribs is None or attr in attribs:
+                    if (attribs is None or str(attr).lower() in attribs
+                            or attr in attribs):
                         if attr in d:
                             d[attr] = d[attr] + "," + m.group('value')
                         else:
