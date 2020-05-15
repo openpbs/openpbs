3f6cd54dbb21a84d5e4506ea16160f8e53f6b639
Sanidhya <sandisamp@gmail.com>
2020-05-15 13:46:58 +0530

    Import package building functionality from docker folder to ci (#1728)

---
+++ ci/build-pbs-packages.sh
@@ -0,0 +1,46 @@
+#! /bin/bash -xe
+
+. /etc/os-release
+
+pbsdir=/pbssrc
+rpm_dir=/root/rpmbuild
+
+mkdir -p /src/packages
+mkdir -p ${rpm_dir}/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
+
+cp -r $pbsdir /tmp/pbs
+cd /tmp/pbs
+./autogen.sh
+cflags="-g -O2 -Wall -Werror"
+if [ "x${ID}" == "xdebian" -o "x${ID}" == "xubuntu" ]; then
+	cflags="${cflags} -Wno-unused-result"
+fi
+./configure CFLAGS="${cflags}" --prefix=/opt/pbs --enable-ptl
+make dist
+cp pbspro-*.tar.gz ${rpm_dir}/SOURCES
+cp pbspro-rpmlintrc ${rpm_dir}/SOURCES
+cp pbspro.spec ${rpm_dir}/SPECS
+cd ${rpm_dir}/SPECS
+if [ "x${ID}" == "xdebian" -o "x${ID}" == "xubuntu" ]; then
+	rpmbuild -ba --nodeps pbspro.spec --with ptl
+else
+	rpmbuild -ba pbspro.spec --with ptl
+fi
+cd /src/packages/
+rm -rf ./*
+mv ${rpm_dir}/RPMS/*/*pbs* /src/packages/
+mv ${rpm_dir}/SRPMS/*pbs* /src/packages/
+rm -rf /tmp/pbs
+
+if [ "x${ID}" == "xdebian" -o "x${ID}" == "xubuntu" ]; then
+	target_arch=$(dpkg --print-architecture)
+	fakeroot alien --to-deb --scripts --target=${target_arch} ./*pbs*.rpm
+	errcode=$?
+	if [ $errcode -ne 0 ]; then
+		echo "ERROR: Conversion of RPM to DEB FAILED"
+		exit 1
+	fi
+	rm -rf *pbs*.rpm
+fi
+
+chmod 755 *pbs*
