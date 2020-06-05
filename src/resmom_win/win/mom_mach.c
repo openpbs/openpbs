a6a4c39d25fb86c10ad6fbf413dbf2a2c11f5042
npadole20 <nehaisat10@gmail.com>
2020-06-05 22:25:08 +0530

    Reverted some incorrect changes from previous commit

--- src/resmom_win/win/mom_mach.c
+++ src/resmom_win/win/mom_mach.c
@@ -610,7 +610,7 @@ mem_sum(job *pjob)
 			ret = IsProcessInJob(ptask->ti_hProc, pjob->ji_hJob, &is_process_in_job);
 			if (!ret)
 				log_eventf(PBSEVENT_DEBUG3, PBS_EVENTCLASS_SERVER, LOG_DEBUG, __func__,
-					"IsProcessInJob failed for %s job %d process with errno %lu", pjob->ji_hJob, ptask->ti_hProc, GetLastError());
+					"IsProcessInJob failed for %d job %d process with errno %lu", pjob->ji_hJob, ptask->ti_hProc, GetLastError());
 
 			/* account for processes that are not part of the Windows job object */
 			if (is_process_in_job == FALSE) {
@@ -668,7 +668,7 @@ cput_sum(job *pjob)
 			ret = IsProcessInJob(ptask->ti_hProc, pjob->ji_hJob, &is_process_in_job);
 			if (!ret)
 				log_eventf(PBSEVENT_DEBUG3, PBS_EVENTCLASS_SERVER, LOG_DEBUG, __func__,
-					"IsProcessInJob failed for %s job %d process with errno %lu", pjob->ji_hJob, ptask->ti_hProc, GetLastError());
+					"IsProcessInJob failed for %d job %d process with errno %lu", pjob->ji_hJob, ptask->ti_hProc, GetLastError());
 
 			/*
 			 * check if the processes is not part of the Windows job object due to pbs_attach
@@ -1183,7 +1183,7 @@ cput_job(char *jobid)
 			ret = IsProcessInJob(ptask->ti_hProc, pjob->ji_hJob, &is_process_in_job);
 			if (!ret)
 				log_eventf(PBSEVENT_DEBUG3, PBS_EVENTCLASS_SERVER, LOG_DEBUG, __func__,
-					"IsProcessInJob failed for %s job %d process with errno %lu", pjob->ji_hJob, ptask->ti_hProc, GetLastError());
+					"IsProcessInJob failed for %d job %d process with errno %lu", pjob->ji_hJob, ptask->ti_hProc, GetLastError());
 
 			/*
 			 * check if the processes is not part of the job object due to pbs_attach,
