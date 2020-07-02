3fea657914647357ad635a7cc131afd00e7cab0d
arungrover <arun.grover@altair.com>
2020-07-02 15:54:10 -0700

    Addressed Bhroam's review comments

--- src/scheduler/node_info.c
+++ src/scheduler/node_info.c
@@ -1847,7 +1847,7 @@ collect_resvs_on_nodes(node_info **ninfo_arr, resource_resv **resresv_arr, int s
  * @param[in]	ninfo	-	the nodes to collect for
  * @param[in]	resresv_arr	-	the array of jobs to consider
  * @param[in]	size	-	the size (in number of pointers) of the job arrays
- * @param[in]   flag	-	flag to indicate whether to do ghost job detection
+ * @param[in]	flags	-	to indicate whether to do ghost job detection
  *
  * @retval	1	: upon success
  * @retval	2	: if a job reported on nodes was not found in the job arrays
@@ -1855,7 +1855,7 @@ collect_resvs_on_nodes(node_info **ninfo_arr, resource_resv **resresv_arr, int s
  *
  */
 int
-collect_jobs_on_nodes(node_info **ninfo_arr, resource_resv **resresv_arr, int size, int flag)
+collect_jobs_on_nodes(node_info **ninfo_arr, resource_resv **resresv_arr, int size, enum misc_constants flags)
 {
 	char *ptr;		/* used to find the '/' in the jobs array */
 	resource_resv *job;	/* find the job from the jobs array */
@@ -1883,7 +1883,7 @@ collect_jobs_on_nodes(node_info **ninfo_arr, resource_resv **resresv_arr, int si
 			/* If there are no running jobs in the list and node reports a running job,
 			 * mark that the node has ghost job
 			 */
-			if (size == 0 && flag == DETECT_GHOST_JOBS) {
+			if (size == 0 && flags == DETECT_GHOST_JOBS) {
 				ninfo_arr[i]->has_ghost_job = 1;
 				log_event(PBSEVENT_DEBUG2, PBS_EVENTCLASS_NODE, LOG_DEBUG, ninfo_arr[i]->name,
 					  "Jobs reported running on node no longer exists or are not in running state");
@@ -1927,7 +1927,7 @@ collect_jobs_on_nodes(node_info **ninfo_arr, resource_resv **resresv_arr, int si
 						/* make the job array searchable with find_resource_resv */
 						ninfo_arr[i]->job_arr[k] = NULL;
 					}
-				} else if (flag == DETECT_GHOST_JOBS) {
+				} else if (flags == DETECT_GHOST_JOBS) {
 					/* Race Condition occurred: nodes were queried when a job existed.
 					 * Jobs were queried when the job no longer existed.  Make note
 					 * of it on the job so the node's resources_assigned values can be
--- src/scheduler/node_info.h
+++ src/scheduler/node_info.h
@@ -46,9 +46,6 @@ extern "C" {
 #include "data_types.h"
 #include <pbs_ifl.h>
 
-#define  DONT_DETECT_GHOST_JOBS	    0
-#define  DETECT_GHOST_JOBS	    1
-
 void query_node_info_chunk(th_data_query_ninfo *data);
 
 /*
@@ -148,7 +145,7 @@ int set_node_type(node_info *ninfo, char *ntype);
  *      collect_jobs_on_nodes - collect all the jobs in the job array on the
  *                              nodes
  */
-int collect_jobs_on_nodes(node_info **ninfo_arr, resource_resv **resresv_arr, int size, int flag);
+int collect_jobs_on_nodes(node_info **ninfo_arr, resource_resv **resresv_arr, int size, enum misc_constants flags);
 
 /*
  *      collect_resvs_on_nodes - collect all the running resvs in the resv array
