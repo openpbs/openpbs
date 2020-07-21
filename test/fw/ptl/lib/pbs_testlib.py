0df7a3516d7b1561ec8d443bb7d5223d8590949e
vccardenas <vcc@altair.com>
2020-07-21 15:13:41 -0700

    added mom restart in delete_vnode_def()

--- test/fw/ptl/lib/pbs_testlib.py
+++ test/fw/ptl/lib/pbs_testlib.py
@@ -13867,13 +13867,15 @@ class MoM(PBSService):
         else:
             return False
 
-    def delete_vnode_defs(self, vdefname=None):
+    def delete_vnode_defs(self, vdefname=None, restart=True):
         """
         delete vnode definition(s) on this MoM
 
         :param vdefname: name of a vnode definition file to delete,
                          if None all vnode definitions are deleted
         :type vdefname: str
+        :param restart: If True, restart the MoM. Default is True
+        :type restart: bool
         :returns: True if delete succeed otherwise False
         """
         cmd = [os.path.join(self.pbs_conf['PBS_EXEC'], 'sbin', 'pbs_mom')]
@@ -13898,6 +13900,8 @@ class MoM(PBSService):
                         return False
                     else:
                         rv = True
+        if restart:
+            self.restart()
         return rv
 
     def has_pelog(self, filename=None):
