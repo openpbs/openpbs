7ca951be5be108fb0b1397658c339c42d5cd6095
vccardenas <vcc@altair.com>
2020-07-22 14:39:02 -0700

    cpuset: moved create of vnode after mom restart

--- test/fw/ptl/utils/pbs_testsuite.py
+++ test/fw/ptl/utils/pbs_testsuite.py
@@ -1462,7 +1462,6 @@ class PBSTestSuite(unittest.TestCase):
             self.assertTrue(mom.isUp(), msg)
         mom.pbs_version()
         restart = False
-        enabled_cpuset = False
         if ((self.revert_to_defaults and self.mom_revert_to_defaults and
              mom.revert_to_default) or force):
             # no need to delete vnodes as it is already deleted in
@@ -1476,25 +1475,6 @@ class PBSTestSuite(unittest.TestCase):
             if 'clienthost' in self.conf:
                 conf.update({'$clienthost': self.conf['clienthost']})
             mom.apply_config(conf=conf, hup=False, restart=False)
-            if mom.is_cpuset_mom():
-                self.server.manager(MGR_CMD_CREATE, NODE, None, mom.shortname)
-                # In order to avoid intermingling CF/HK/PY file copies from the
-                # create node and those caused by the following call, wait
-                # until the dialogue between MoM and the server is complete
-                time.sleep(4)
-                just_before_enable_cgroup_cset = time.time()
-                mom.enable_cgroup_cset()
-                # a high max_attempts is needed to tolerate delay receiving
-                # hook-related files, due to temporary network interruptions
-                mom.log_match('pbs_cgroups.CF;copy hook-related '
-                              'file request received', max_attempts=120,
-                              starttime=just_before_enable_cgroup_cset-1,
-                              interval=1)
-                # Make sure that the MoM will generate per-NUMA node vnodes
-                # when the natural node is created below
-                # HUP may not be enough if exechost_startup is delayed
-                restart = True
-                enabled_cpuset = True
         if restart:
             mom.restart()
         else:
@@ -1502,16 +1482,27 @@ class PBSTestSuite(unittest.TestCase):
         if not mom.isUp():
             self.logger.error('mom ' + mom.shortname + ' is down after revert')
         a = {'state': 'free'}
-        if enabled_cpuset:
-            # Checking whether the CF file was copied really belongs in code
-            # that changes the config file -- i.e. after enable_cgroup_cset
-            # called above. We're not sure it is always called here,
-            # since that call is in an if
+        self.server.manager(MGR_CMD_CREATE, NODE, None, mom.shortname)
+        if mom.is_cpuset_mom():
+            # In order to avoid intermingling CF/HK/PY file copies from the
+            # create node and those caused by the following call, wait
+            # until the dialogue between MoM and the server is complete
+            time.sleep(4)
+            just_before_enable_cgroup_cset = time.time()
+            mom.enable_cgroup_cset()
+            # a high max_attempts is needed to tolerate delay receiving
+            # hook-related files, due to temporary network interruptions
+            mom.log_match('pbs_cgroups.CF;copy hook-related '
+                          'file request received', max_attempts=120,
+                          starttime=just_before_enable_cgroup_cset-1,
+                          interval=1)
+            # Make sure that the MoM will generate per-NUMA node vnodes
+            # when the natural node was created above.
+            # HUP may not be enough if exechost_startup is delayed
             time.sleep(2)
             mom.signal('-HUP')
             self.server.expect(NODE, a, id=mom.shortname + '[0]', interval=1)
         else:
-            self.server.manager(MGR_CMD_CREATE, NODE, id=mom.shortname)
             self.server.expect(NODE, a, id=mom.shortname, interval=1)
         return mom
 
