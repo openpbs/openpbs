0fcb64ed74a714d687f90c231bb909e18814f35f
arungrover <arun.grover@altair.com>
2020-07-23 15:20:45 -0700

    changed a comment

--- src/include/reservation.h
+++ src/include/reservation.h
@@ -118,7 +118,7 @@ struct resv_alter {
 	char *ra_select;
 	long ra_state;
 	unsigned long ra_flags;
-	char *ra_select_revert; /* used to revert select for future occurrences*/
+	char *ra_select_revert; /* used to revert select for future occurrences */
 };
 
 /*
