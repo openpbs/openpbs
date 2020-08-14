a385a8ff6a72da85df2fb7e8a30f3e0efb207439
Shilpa Kodli <shilpa.kodli@altair.com>
2020-08-14 17:22:52 +0530

    Addressing Lisa's comments

--- test/tests/functional/pbs_alps_release_tunables.py
+++ test/tests/functional/pbs_alps_release_tunables.py
@@ -3,37 +3,39 @@
 # Copyright (C) 1994-2020 Altair Engineering, Inc.
 # For more information, contact Altair at www.altair.com.
 #
-# This file is part of the PBS Professional ("PBS Pro") software.
+# This file is part of both the OpenPBS software ("OpenPBS")
+# and the PBS Professional ("PBS Pro") software.
 #
 # Open Source License Information:
 #
-# PBS Pro is free software. You can redistribute it and/or modify it under the
-# terms of the GNU Affero General Public License as published by the Free
-# Software Foundation, either version 3 of the License, or (at your option) any
-# later version.
+# OpenPBS is free software. You can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
 #
-# PBS Pro is distributed in the hope that it will be useful, but WITHOUT ANY
-# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE.
-# See the GNU Affero General Public License for more details.
+# OpenPBS is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public
+# License for more details.
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 # Commercial License Information:
 #
-# For a copy of the commercial license terms and conditions,
-# go to: (http://www.pbspro.com/UserArea/agreement.html)
-# or contact the Altair Legal Department.
+# PBS Pro is commercially licensed software that shares a common core with
+# the OpenPBS software.  For a copy of the commercial license terms and
+# conditions, go to: (http://www.pbspro.com/agreement.html) or contact the
+# Altair Legal Department.
 #
-# Altair’s dual-license business model allows companies, individuals, and
-# organizations to create proprietary derivative works of PBS Pro and
+# Altair's dual-license business model allows companies, individuals, and
+# organizations to create proprietary derivative works of OpenPBS and
 # distribute them - whether embedded or bundled with other software -
 # under a commercial license agreement.
 #
-# Use of Altair’s trademarks, including but not limited to "PBS™",
-# "PBS Professional®", and "PBS Pro™" and Altair’s logos is subject to Altair's
-# trademark licensing policies.
+# Use of Altair's trademarks, including but not limited to "PBS™",
+# "OpenPBS®", "PBS Professional®", and "PBS Pro™" and Altair's logos is
+# subject to Altair's trademark licensing policies.
 
 from tests.functional import *
 import math
@@ -98,7 +100,7 @@ class TestCrayAlpsReleaseTunables(TestFunctional):
             for data in out[1:]:
                 time_current = self.get_epoch(data[1])
                 fail_msg = "alps_release_wait_time not working"
-                self.assertGreaterEqual(int(time_current - time_prev),
+                self.assertGreaterEqual(time_current - time_prev,
                                         math.floor(arwt),
                                         msg=fail_msg)
                 time_prev = time_current
@@ -152,7 +154,7 @@ class TestCrayAlpsReleaseTunables(TestFunctional):
                 time_prev = self.get_epoch(out[0][1])
                 for data in out[1:]:
                     time_current = self.get_epoch(data[1])
-                    self.assertLessEqual(int(time_current - time_prev),
+                    self.assertLessEqual(time_current - time_prev,
                                          max_delay,
                                          msg="alps_release_jitter not working")
                     time_prev = time_current
