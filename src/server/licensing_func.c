f310fe1ee7b85c349eb0c9ff39e2dd6032bc6511
Jitendra Kumar Rajak <jitendrakumar.rajak@altair.com>
2020-10-02 09:48:36 +0530

    removing the segmentation fault (#2054)

--- src/server/licensing_func.c
+++ src/server/licensing_func.c
@@ -544,6 +544,9 @@ license_one_node(pbsnode *pnode)
 				update_license_highuse();
 			} else {
 				add_to_unlicensed_node_list(pnode);
+				if (is_attr_set(&(pnode->nd_attr[ND_ATR_LicenseInfo]))) {
+					licensing_control.licenses_total_needed += get_attr_l(&(pnode->nd_attr[ND_ATR_LicenseInfo]));
+				}
 				if (get_more_licenses_task)
 					delete_task(get_more_licenses_task);
 				get_more_licenses_task = set_task(WORK_Timed, time_now + 2, get_more_licenses, NULL);
@@ -691,11 +694,13 @@ init_licensing(struct work_task *ptask)
 	}
 	for (i = 0; i < svr_totnodes; i++) {
 	 	clear_node_lic_attrs(pbsndlist[i], 0);
-		if (pbsndlist[i]->nd_attr[ND_ATR_LicenseInfo].at_val.at_long) {
-			licensing_control.licenses_total_needed += pbsndlist[i]->nd_attr[ND_ATR_LicenseInfo].at_val.at_long;
+		if (is_attr_set(&(pbsndlist[i]->nd_attr[ND_ATR_LicenseInfo]))) {
+			licensing_control.licenses_total_needed += get_attr_l(&(pbsndlist[i]->nd_attr[ND_ATR_LicenseInfo]));
 		} else {
-			set_node_lic_info_attr(pbsndlist[i]);
-			licensing_control.licenses_total_needed += pbsndlist[i]->nd_attr[ND_ATR_LicenseInfo].at_val.at_long;
+			if (pbsndlist[i]->nd_lic_info != NULL) {
+				set_node_lic_info_attr(pbsndlist[i]);
+				licensing_control.licenses_total_needed += get_attr_l(&(pbsndlist[i]->nd_attr[ND_ATR_LicenseInfo]));
+			}
 		}
 		add_to_unlicensed_node_list(pbsndlist[i]);
 	}
