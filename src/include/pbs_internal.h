4895b8376e56b95284818e598b178414a0083cef
arungrover <arun.grover@altair.com>
2020-10-09 14:23:19 -0700

    removed unnecessary enum

--- src/include/pbs_internal.h
+++ src/include/pbs_internal.h
@@ -355,12 +355,6 @@ enum accrue_types {
 	JOB_EXIT
 };
 
-/* flag for cvtdate */
-enum cvtdate_flags {
-	DONT_ADVANCE_DAY = 0x01,
-	ADVANCE_DAY = 0x02
-};
-
 #define	ACCRUE_NEW	"0"
 #define	ACCRUE_INEL	"1"
 #define	ACCRUE_ELIG	"2"
@@ -464,7 +458,7 @@ DECLDIR char    *pbs_ispbsdir(char *, char *);
 DECLDIR int      pbs_isjobid(char *);
 DECLDIR int      check_job_name(char *, int);
 DECLDIR int      chk_Jrange(char *);
-DECLDIR time_t   cvtdate(char *, int);
+DECLDIR time_t   cvtdate(char *);
 DECLDIR int      locate_job(char *, char *, char *);
 DECLDIR int      parse_destination_id(char *, char **, char **);
 DECLDIR int      parse_at_list(char *, int, int);
