045af1c997038f7cc0c994754e8b12be23255969
Vincent Stumpf <vincents.995@gmail.com>
2020-11-17 10:02:44 -0800

    One more line

--- src/scheduler/prime.cpp
+++ src/scheduler/prime.cpp
@@ -612,7 +612,7 @@ end_prime_status_rec(time_t start, time_t date,
 			return date;
 		/* Non-primetime will start later today, return the scheduled time. */
 		return date + (conf.prime[day][NON_PRIME].hour - tmptr->tm_hour) * 3600
-		+ (conf.prime[day][NON_PRIME].min - tmptr->tm_min) * 60
+		+ (static_cast<int>(conf.prime[day][NON_PRIME].min) - tmptr->tm_min) * 60
 		- tmptr->tm_sec;
 	}
 	else {
