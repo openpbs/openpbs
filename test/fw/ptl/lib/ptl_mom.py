6cf6cc474e73c6ce77c93188d15b3b7556b9a809
vccardenas <vcc@altair.com>
2020-11-18 11:14:02 -0800

    shortname for pbsnodes

--- test/fw/ptl/lib/ptl_mom.py
+++ test/fw/ptl/lib/ptl_mom.py
@@ -1391,7 +1391,7 @@ class MoM(PBSService):
                  'content-encoding': 'default',
                  'input-file': path}
             # check that mom is ready before importing hook
-            self.server.expect(NODE, {'state': 'free'}, id=self.hostname)
+            self.server.expect(NODE, {'state': 'free'}, id=self.shortname)
             self.server.manager(MGR_CMD_IMPORT, HOOK, a,
                                 'pbs_cgroups')
             os.remove(path)
