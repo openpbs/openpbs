89eee7b9073fe0dd16a158a03a978c83def19a1f
Arun Grover <arun.grover@altair.com>
2020-12-16 15:19:06 -0800

    Added a check for valid recurrence frequency

--- src/lib/Libutil/pbs_ical.c
+++ src/lib/Libutil/pbs_ical.c
@@ -322,6 +322,11 @@ check_rrule(char *rrule, time_t dtstart, time_t dtend, char *tz, int *err_code)
 		}
 	}
 
+        /* Check if frequency was correctly set */
+	if (rt.freq == ICAL_NO_RECURRENCE) {
+		*err_code = PBSE_BAD_RRULE_SYNTAX;
+		return 0;
+	}
 	/* Check if the rest of the by_* rules are defined
 	 * and valid.
 	 * currently no support for
