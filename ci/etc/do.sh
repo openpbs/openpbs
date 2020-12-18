ddc9332ff6a70b4fca417ecbb53f13d47732a46b
kumar-jak <69527441+kumar-jak@users.noreply.github.com>
2020-12-18 10:36:11 +0530

    Update do.sh

--- ci/etc/do.sh
+++ ci/etc/do.sh
@@ -283,5 +283,14 @@ if [ "x${IS_CI_BUILD}" != "x1" ]; then
   cd /opt/ptl/tests/
   env
   locale
+ls -l /usr/lib/man-db/zsoelim
+ls -l /usr/lib/man-db/manconv
+which zsoelim
+which manconv
+which preconv
+which tbl
+which nroff
+man /opt/pbs/bin/pbsnodes
+
   pbs_benchpress -t SmokeTest.test_man_pages 
 fi
