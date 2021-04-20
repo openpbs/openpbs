b40f7292d45736000988dc3aac58f2e6bf98b75c
Voltaire C. Cardenas <vcc@altair.com>
2021-04-19 21:20:54 -0700

    In revert_mom() allow time for mom to sync with server prior to create node. (#2347)

--- test/fw/ptl/utils/pbs_testsuite.py
+++ test/fw/ptl/utils/pbs_testsuite.py
@@ -1531,6 +1531,9 @@ class PBSTestSuite(unittest.TestCase):
             mom.signal('-HUP')
         if not mom.isUp():
             self.logger.error('mom ' + mom.shortname + ' is down after revert')
+        # give mom enough time to network sync with the server on cpuset system
+        if enabled_cpuset:
+            time.sleep(4)
         a = {'state': 'free'}
         self.server.manager(MGR_CMD_CREATE, NODE, None, mom.shortname)
         if enabled_cpuset:
