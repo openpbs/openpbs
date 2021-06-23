94ecfb3ddc1f41cd7d1d75ec9b6104f7bf864c2d
Dinesh Joshi <dineshjoshi1306@gmail.com>
2021-06-23 23:25:40 +0530

    fixed test 'test_auto_sched_cycle_trigger' from 'TestVerifyLogOutput' failed in assertion error. (#2427)

--- test/tests/functional/pbs_verify_log_output.py
+++ test/tests/functional/pbs_verify_log_output.py
@@ -145,15 +145,10 @@ class TestVerifyLogOutput(TestFunctional):
         PBSInitServices().restart()
 
         if self.server.isUp() and self.scheduler.isUp():
-            try:
-                self.scheduler.log_match("Req;;Starting Scheduling Cycle",
-                                         max_attempts=10,
-                                         starttime=started_time)
-                self.scheduler.log_match("Req;;Leaving Scheduling Cycle",
-                                         max_attempts=10,
-                                         starttime=started_time)
-            except PtlLogMatchError:
-                self.assertFalse(True)
+            self.scheduler.log_match("Req;;Starting Scheduling Cycle",
+                                     starttime=started_time)
+            self.scheduler.log_match("Req;;Leaving Scheduling Cycle",
+                                     starttime=started_time)
 
     def test_supported_auth_method_msgs(self):
         """
