4e1d90114a7e618213e3558e74ac0dd6264ddf26
Václav Chlumský <chlumskyvaclav@gmail.com>
2023-07-14 17:00:57 +0200

    terminate the afslog process if a job's task fails to start (#2600)

--- src/resmom/start_exec.c
+++ src/resmom/start_exec.c
@@ -1768,6 +1768,9 @@ record_finish_exec(int sd)
 		if (sjr.sj_reservation == -1)
 			call_hup = HUP_INIT;
 #endif
+#if defined(PBS_SECURITY) && (PBS_SECURITY == KRB5)
+		AFSLOG_TERM(ptask);
+#endif
 		(void) sprintf(log_buffer, "job not started, %s %d",
 			       (sjr.sj_code == JOB_EXEC_RETRY) ? "Retry" : "Failure", sjr.sj_code);
 		exec_bail(pjob, sjr.sj_code, log_buffer);
@@ -4671,6 +4674,9 @@ start_process(task *ptask, char **argv, char **envp, bool nodemux)
 		 */
 		set_globid(pjob, &sjr);
 		if (sjr.sj_code < 0) {
+#if defined(PBS_SECURITY) && (PBS_SECURITY == KRB5)
+			AFSLOG_TERM(ptask);
+#endif
 			(void) sprintf(log_buffer, "task not started, %s %s %d",
 				       (sjr.sj_code == JOB_EXEC_RETRY) ? "Retry" : "Failure",
 				       argv[0],
